cf3080ded99a81f86cc9c594890c1b813f793d0b
mrsaxena77 <67632399+mrsaxena77@users.noreply.github.com>
2023-04-04 00:50:09 +0530

    Update toDo.js

--- toDo.js
+++ toDo.js
@@ -5,18 +5,22 @@ let subBtn = document.querySelector(".subBtn");
 
 
 // we want the content which is our input on the current ui after clicking on submit button. So, put an eventListener on submit button with function (e).
-subBtn.addEventListener("click", function (e) {
+subBtn.addEventListener("click", function () {
 
-// in line no. 9: the value or the input we have, has stored in variable contValue.
+// in line no. 11: the value or the input we have, has stored in variable contValue.
     let contValue = content.value;
-    if (contValue == " ") {        // this line is checking that what is the input value ? if it is blank (execute next line).
-        alert("Please Enter Content!");       // this line gives an alert when tha input is blank and return.
-        return;
+    if (contValue == "") {        // this line is checking that what is the input value ? if it is blank (execute next line).
+        alert("Empty Content Box!");
+        return;  // this line gives an alert when tha input is blank and return.
     }
     else {
-        let list = document.createElement("list");  // here we creating a new element in the grid of ui.
-        list.innerHTML = contValue;     // and storing the content in the new element.
-        data.append(list);              // appending with an element of HTML.
+        let li = document.createElement("li");  // here we creating a new element in the grid of ui.
+        li.innerHTML = contValue;     // and storing the content in the new element.
+        data.append(li);              // appending with an element of HTML.
 
     }
+
+
+
+    content.value = "";
 });
